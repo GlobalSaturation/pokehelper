83997de692c5fe9a20c7fcca69530609ed4e8d70
GlobalSaturation <87565097+GlobalSaturation@users.noreply.github.com>
2025-07-21 14:22:58 -0700

    Add all types and a simple color for each button

--- javascript.js
+++ javascript.js
@@ -1,35 +1,132 @@
 //define master type obj that stores a single type obj per key
 const types = {
 	grass: {
+		color: "#3fa129",
 		weak: ["flying", "poison", "bug", "fire", "ice"],
 		resist: ["ground", "water", "grass", "electric"],
 		immune: [],
 	},
 	water: {
+		color: "#2980ef",
 		weak: ["grass", "electric"],
 		resist: ["fire", "water", "ice", "steel"],
 		immune: [],
 	},
 	fire: {
+		color: "#e62829",
 		weak: ["ground", "rock", "water"],
 		resist: ["bug", "fire", "grass", "steel", "ice", "fairy"],
 		immune: [],
+	},
+	normal: {
+		color: "#9fa19f",
+		weak: ["fighting"],
+		resist: [],
+		immune: ["ghost"],
+	},
+	fighting: {
+		color: "#ff8000",
+		weak: ["flying", "psychic", "fairy"],
+		resist: ["rock", "bug", "dark"],
+		immune: [],
+	},
+	flying: {
+		color: "#81b9ef",
+		weak: ["rock", "electric", "ice"],
+		resist: ["fighting", "bug", "grass"],
+		immune: ["ground"],
+	},
+	poison: {
+		color: "#9141cb",
+		weak: ["ground", "psychic"],
+		resist: ["fighting", "poison", "grass", "bug", "fairy"],
+		immune: [],
+	},
+	ground: {
+		color: "#915121",
+		weak: ["water", "grass", "ice"],
+		resist: ["poison", "rock"],
+		immune: ["electric"],
+	},
+	rock: {
+		color: "#afa981",
+		weak: ["fighting", "ground", "water", "grass", "steel"],
+		resist: ["normal", "flying", "poison", "fire"],
+		immune: [],
+	},
+	bug: {
+		color: "#91a119",
+		weak: ["flying", "rock", "fire"],
+		resist: ["fighting", "ground", "grass"],
+		immune: [],
+	},
+	ghost: {
+		color: "#704170",
+		weak: ["ghost", "dark"],
+		resist: ["poison", "bug"],
+		immune: ["normal", "fighting"],
+	},
+	steel: {
+		color: "#60a1b8",
+		weak: ["fighting", "ground", "fire"],
+		resist: ["normal", "flying", "rock", "bug", "steel", "grass", "psychic", "ice", "dragon", "fairy"],
+		immune: ["poison"],
+	},
+	electric: {
+		color: "#fac000",
+		weak: ["ground"],
+		resist: ["flying", "electric", "steel"],
+		immune: [],
+	},
+	psychic: {
+		color: "#ef4179",
+		weak: ["bug", "ghost", "dark"],
+		resist: ["fighting", "psychic"],
+		immune: [],
+	},
+	ice: {
+		color: "#3dcef3",
+		weak: ["fighting", "rock", "fire", "steel"],
+		resist: ["ice"],
+		immune: [],
+	},
+	dragon: {
+		color: "#5060e1",
+		weak: ["ice", "dragon", "fairy"],
+		resist: ["fire", "water", "grass", "electric"],
+		immune: [],
+	},
+	dark: {
+		color: "#624d4e",
+		weak: ["fighting", "bug", "fairy"],
+		resist: ["ghost", "dark"],
+		immune: ["psychic"],
+	},
+	fairy: {
+		color: "#ef70ef",
+		weak: ["poison", "steel"],
+		resist: ["fighting", "bug", "dark"],
+		immune: ["dragon"],
 	},
 }
 
 //make a global queue that stores the first two choices and removes the first thing if capacity is exceeded
 let typeQueue = [];
+//also this so we can access the button ref that corresponds to the types in the typeQueue
+let btnQueue = [];
 
-function addToQueue(typeName) {
+function addToQueue(btn, typeName) {
 	//if we've already added this type, remove it
 	if (typeQueue.includes(typeName)) {
 		const toRemove = typeQueue.findIndex((item) => item === typeName);
 		typeQueue.splice(toRemove, 1);
+		btnQueue.splice(toRemove, 1);
 	}
 	else {
 		typeQueue.push(typeName);
 		if (typeQueue.length > 2) {
 			typeQueue.shift();//take out first element in array
+			btnQueue.shift();//take out first element in array
 		}
 	}
 	console.log(typeQueue);
@@ -100,9 +197,8 @@ function calculateTypes(typeArray) {
 const container = document.querySelector("#buttons");
 for (let type in types) {
 	const btn = document.createElement("button");
-	btn.addEventListener("click", () => addToQueue(type));
-	//console.log(types[type]);
-	//just type on its own is a string
+	btn.addEventListener("click", (e) => addToQueue(e.target,type));
 	btn.innerText = type;
+	btn.style.backgroundColor = types[type].color;
 	container.appendChild(btn);
 }
